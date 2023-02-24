092474c0b6ae96ce47a5c37a5d65db9702ac4d49
skim <rumex13@naver.com>
2023-02-24 14:37:38 +0900

    add library

--- library/HC-SR04_timer3LED.py
+++ library/HC-SR04_timer3LED.py
@@ -1,23 +1,7 @@
 from __future__ import print_function
 import time
 import RPi.GPIO as GPIO
-import RPi.GPIO as IoPort
 
-def State_1(Delay):
-    IoPort.output(GPIO_LED2, True)
-    IoPort.output(GPIO_LED3, False)
-    time.sleep(Delay)
-
-def State_2(Delay):
-    IoPort.output(GPIO_LED2, True)
-    IoPort.output(GPIO_LED, False)
-    time.sleep(Delay)
-    
-def State_3(Delay):
-    IoPort.output(GPIO_LED3, True)
-    IoPort.output(GPIO_LED2, False)
-    time.sleep(Delay)
-   
 def measure():
     GPIO.output(GPIO_TRIGGER, True)
     time.sleep(0.00001)
@@ -45,32 +29,13 @@ def measure_average():
     distance = distance / 3
     return distance
 
+GPIO.setmode(GPIO.BCM)
+
 GPIO_TRIGGER = 17
 GPIO_ECHO = 18
 GPIO_LED = 5
 GPIO_LED2 = 6
 GPIO_LED3 = 19
-
-#IoPort.setmode(IoPort.BCM)
-#IoPort.setup(GPIO_LED, IoPort.OUT)
-#IoPort.setup(GPIO_LED2, IoPort.OUT)
-#IoPort.setup(GPIO_LED3, IoPort.OUT)
-#time.sleep(0.8)
-#no = 0.1
-
-#while no < 1:
-    #State_1(0.8)
-    #State_2(0.8)
-    #State_3(0.8)
-    #no += 0.1
-
-GPIO.setmode(GPIO.BCM)
-
-#GPIO_TRIGGER = 17
-#GPIO_ECHO = 18
-#GPIO_LED = 5
-#GPIO_LED2 = 6
-#GPIO_LED3 = 19
 
 print("Ultrasonic Measurement")
 
@@ -80,15 +45,7 @@ GPIO.setup(GPIO_LED3, GPIO.OUT)
 GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
 GPIO.setup(GPIO_ECHO, GPIO.IN)
 
-#GPIO.output(GPIO_TRIGGER, False)
-time.sleep(0.8)
-no = 0.1
-
-#while no < 1:
-    #State_1(0.8)
-    #State_2(0.8)
-    #State_3(0.8)
-    #no += 0.1
+GPIO.output(GPIO_TRIGGER, False)
 
 try:
     
@@ -109,12 +66,6 @@ try:
             GPIO.output(GPIO_LED, GPIO.LOW)
             GPIO.output(GPIO_LED2, GPIO.LOW)
             GPIO.output(GPIO_LED3, GPIO.LOW)
-            
-        while no < 1:
-            State_1(0.8)
-            State_2(0.8)
-            State_3(0.8)
-            no += 0.1
             
 except KeyboardInterrupt:
     GPIO.cleanup()
