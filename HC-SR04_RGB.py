3a92389b6982002176d125ae6ea567c1b44d3c6c
skim <rumex13@naver.com>
2023-02-26 13:36:15 +0900

    resolve exception

--- HC-SR04_RGB.py
+++ HC-SR04_RGB.py
@@ -13,34 +13,37 @@ GPIO.setup(LED_R, GPIO.OUT)
 GPIO.setup(LED_B, GPIO.OUT)
 
 def getDistance():
-    GPIO.output(TRIG, False)
-    time.sleep(1)
-    GPIO.output(TRIG, True)
-    time.sleep(0.00001)
-    GPIO.output(TRIG, False)
-    
-    while GPIO.input(ECHO) == 0:
-        pulse_start = time.time()
-    while GPIO.input(ECHO) == 1:
-        pulse_end = time.time()
+    try:
+        GPIO.output(TRIG, False)
+        time.sleep(1)
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
         
-    pulse_duration = pulse_end-pulse_start
-    distance = round(pulse_duration*17150,2)
-    
-    return distance
+        while GPIO.input(ECHO) == 0:
+            pulse_start = time.time()
+        while GPIO.input(ECHO) == 1:
+            pulse_end = time.time()
+            
+        pulse_duration = pulse_end-pulse_start
+        distance = round(pulse_duration*17150,2)
+        return distance
+    except:
+        return None
 
 if __name__ == '__main__':
     try:
         while True:
             distance_value = getDistance()
-            if distance_value > 2 and distance_value < 35:
-                print("Distance is %.2f cm" %distance_value)
-                GPIO.output(LED_R, GPIO.HIGH)
-                GPIO.output(LED_B, GPIO.LOW)
-            else:
-                print("Out of Range %.2f cm" %distance_value)
-                GPIO.output(LED_B, GPIO.HIGH)
-                GPIO.output(LED_R, GPIO.LOW)
+            if distance_value != None:
+                if distance_value > 2 and distance_value < 35:
+                    print("Distance is %.2f cm" %distance_value)
+                    GPIO.output(LED_R, GPIO.HIGH)
+                    GPIO.output(LED_B, GPIO.LOW)
+                else:
+                    print("Out of Range %.2f cm" %distance_value)
+                    GPIO.output(LED_B, GPIO.HIGH)
+                    GPIO.output(LED_R, GPIO.LOW)
                 
     except KeyboardInterrupt:
         GPIO.output(LED_R, GPIO.LOW)
