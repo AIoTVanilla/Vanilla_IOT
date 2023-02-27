3dbae36c427bf79d2fbe6ba4b632e139df0bf741
skim <rumex13@naver.com>
2023-02-27 17:21:18 +0900

    remove daemon

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ t1 = Thread(target=subprocess.run, args=(["python3", "socket_client.py"],))
 t2 = Thread(target=subprocess.run, args=(["python3", "HC-SR04_RGB.py"],))
 
 t1.start()
-t1.setDaemon(True)
 t2.start()
-t2.setDaemon(True)
 
 t1.join()
 t2.join()
